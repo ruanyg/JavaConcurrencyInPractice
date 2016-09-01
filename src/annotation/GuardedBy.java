39699009500f18ccdb23a2e783e1c2c227ae1631
ruanyg <ruanyaguang@raycloud.com>
2016-09-01 10:38:44 +0800

    使用官方代码示例

--- src/annotation/GuardedBy.java
+++
@@ -1,8 +0,0 @@
-package annotation;
-
-/**
- * Created by ruanyg on 16/8/31.
- */
-public @interface GuardedBy {
-    String value();
-}
--- src/annotation/ThreadSafe.java
+++
@@ -1,7 +0,0 @@
-package annotation;
-
-/**
- * Created by ruanyg on 16/8/31.
- */
-public @interface ThreadSafe {
-}
